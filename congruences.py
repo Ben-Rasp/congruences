fd075f920a17ae996d418def2882268e26adcfe3
Ben-Rasp <104253009+Ben-Rasp@users.noreply.github.com>
2023-11-01 15:00:30 +0100

    Add files via upload

--- congruences.py
+++ congruences.py
@@ -1,78 +1,42 @@
 from turtle import *
 speed(0)        
 
-def dessine_congruences(x0, y0, n, modulo):
-    up(); goto(x0-15, y0-20); down();
-    write(f'Congruences de {n} modulo {modulo}')
-    up();goto(x0,y0); down()
-    positions = []
-    flag=0
-    for x in range(0,modulo):
-        forward(1000*1/modulo)
-        left(360/modulo)
-        dot()
-        positions.append(pos())
-        if modulo < 51:
-            write(flag, align="left", font=("calibri", 12, "normal"))
-        flag += 1
-#    flag = 1
-#    color = "red"
-    for x in range(1,modulo):
-        multiplication = n * x
-        mod = multiplication % modulo
-        modx, mody = positions[mod]
-        up()
-        goto(positions[x])
-        down()
-        goto(positions[mod])
-    return positions
+def dessine_congruences(x0, y0, n, modulo):      ##création de la fonction
+    """Déplace le curseur de turtle vers le début deu schéma, trace un cercle 
+    sur lequel sont répartis les nombres entre 0 et le modulo (c'est à dire tous
+    les 'restes' possibles) puis trace le liens entre les nombres de la table de 
+    multiplication et leur congruence."""
+    up(); goto(x0-25, y0-30); down();            ##le crayon se déplace
+    write(f'Congruences de {n} modulo {modulo}', ##Création du titre de l'image
+         font=('calibri', 14, 'normal'))         
+    up();goto(x0,y0); down()                     ##le crayon se déplace: x0,y0
+    speed(0)                                     ##et il s'abaisse pour écrire
+    positions = []                               ##liste des positions (vide)
 
-##dessine_congruences(-300, 60, 18, 47)
-##dessine_congruences(150, 60, 18, 48)
-##dessine_congruences(-300, -320, 18, 49)
-#dessine_congruences(150, -320, 18, 50)
+    for x in range(0,modulo):                    ##pour chaque nbr du modulo...
+        forward(1500*1/modulo)                   ##je m'avance d'une distance
+                                                 ##proportionnelle (pour rester
+                                                 ##dans le cadre)
+        left(360/modulo)                         ##je tourne ensuite d'un angle
+                                                 ##particulier (360 / modulo)
+        dot()                                    ##je trace un point
+        positions.append(pos())                  ##les coordonnées de ce point
+                                                 ##sont stockées dans positions
+        if modulo < 51:      ##si le modulo est <51, je peux noter les nombres.
+            write(x, align="left", font=("calibri", 12, "normal"))
 
-class Congruences:
-    def __init__(self, nombre, modulo):
-        self.nombre = nombre
-        self.modulo = modulo
-        self.positions = []
+    for x in range(1,modulo):          ##pour chaque nombre de modulo...
+        multiplication = n * x         ##je calcule sa multiplication par n
+        mod = multiplication % modulo  ##je calcule le résultat de cette 
+                                       ##multiplication modulo (le reste)
+        modx, mody = positions[mod]    ##les coord x,y sont celles du point
+                                       ##stocké dans positions à son emplacement
+        up()                           ##je soulève le crayon
+        goto(positions[x])             ##je me déplace vers le point x
+        down()                         ##je baisse le crayon pour tracer
+        goto(positions[mod])           ##je trace le trait vers le modulo de x
 
-    def dessine_cercle(self, xorigine=-100, yorigine=-200):
-        self.xorigine = xorigine
-        self.yorigine = yorigine
-        up(); goto(self.xorigine-25, self.yorigine-30); down();
-        write(f'Congruences de {self.nombre} modulo {self.modulo}',
-              font=("calibri", 14, "normal"))
-        up();goto(self.xorigine, self.yorigine); down()
-        flag = 0
-        for x in range(0,self.modulo):
-            forward(1500*1/self.modulo)
-            left(360/self.modulo)
-            dot()
-            self.positions.append(pos())
-            if self.modulo < 51:
-                write(flag, align="left", font=("calibri", 18, "normal"))
-            flag += 1
-        
-        for x in range(1,self.modulo):
-            multiplication = self.nombre * x
-            mod = multiplication % self.modulo
-            modx, mody = self.positions[mod]
-            up()
-            goto(self.positions[x])
-            down()
-            goto(self.positions[mod])
-    
-    def histogramme(self):
-        mods = [(self.nombre*x)% self.modulo for x in range(1, self.modulo)]
-        self.sommes_congruences= []
-        xaxis = [x for x in range(self.modulo)]
-        for m in xaxis:
-            self.sommes_congruences.append(mods.count(m))
-        self.fig = plt.plot(xaxis, self.sommes_congruences)
-        plt.show()
-        
-C = Congruences(4, 10)
-C.dessine_cercle()
+#MAIN_________________________________________________________________________
 
+if __name__=="__main__":
+    dessine_congruences(-150, -200, 12, 10) #polygone de la table de 12, modulo 4
